2458be406c66954953ecb37ee634916a17ae1083
Yura Kushnir <kushnir.yura@gmail.com>
2019-05-13 15:26:33 +0300

    Updates

---
+++ precache-manifest.21327c4663a9e20444e248972c81db54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36e9b660e44e9ffd6991d17adaa45d4",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "3d1271c62a670b398587",
+    "url": "/2048/static/css/main.eb7cc01b.chunk.css"
+  },
+  {
+    "revision": "2faef6bc8214d1135160",
+    "url": "/2048/static/js/2.f09c5d3d.chunk.js"
+  },
+  {
+    "revision": "3d1271c62a670b398587",
+    "url": "/2048/static/js/main.619208b4.chunk.js"
+  },
+  {
+    "revision": "db00710998a9f0cd2fd8",
+    "url": "/2048/static/js/runtime~main.62169bd9.js"
+  }
+]);
