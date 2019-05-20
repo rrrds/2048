505e77ebcb5091483f39d123bd50b4a2fe7511ac
Yura Kushnir <kushnir.yura@gmail.com>
2019-05-20 14:40:55 +0300

    Updates

---
+++ precache-manifest.0a08667672b58a9e451e16a78cac18bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2055f37d194a30d8385965c04cebc05",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "6c4d0a11f790a07e1890",
+    "url": "/2048/static/css/main.a85a2fab.chunk.css"
+  },
+  {
+    "revision": "790d5b82d854cf650f9b",
+    "url": "/2048/static/js/2.04dc61ca.chunk.js"
+  },
+  {
+    "revision": "6c4d0a11f790a07e1890",
+    "url": "/2048/static/js/main.4854e940.chunk.js"
+  },
+  {
+    "revision": "db00710998a9f0cd2fd8",
+    "url": "/2048/static/js/runtime~main.62169bd9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2048/precache-manifest.0b5b7a9d3770917e149d68a3466ce9d7.js"
+  "/2048/precache-manifest.0a08667672b58a9e451e16a78cac18bf.js"
 );
 
 self.addEventListener('message', (event) => {
